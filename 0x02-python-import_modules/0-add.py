8912599f01f904ab61627f1b81fb2902a1131615
Mesele <getmesy@gmail.com>
2022-12-04 07:18:19 +0000

    push 1-calculation.py and correct 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,10 @@
 # 0_add.py
 
 if __name__ == "__main__":
-    """ Print the sum of a & b."""
+    """ Print the sum of 1 & 2."""
     from add_0 import add
+
     a = 1
     b = 2
+
     print("{} + {} = {}".format(a, b, add(a,b)))
