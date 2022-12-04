1f4d660caced16b2eb6b0eb028c7d245e10df9f5
Mesele <getmesy@gmail.com>
2022-12-04 06:46:01 +0000

    correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+# 0_add.py
 
-from add_0 import add
-
-a = 1
-b = 2
-c = add(a,b)
-print("{} + {} = {}".format(a, b, c))
+if __name__ == "__main__":
+    """ Print the sum of a & b."""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a,b)))
