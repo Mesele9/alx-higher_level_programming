f372912e99e12202dcedb766f16bb16f16b98090
Mesele <getmesy@gmail.com>
2022-12-04 09:42:15 +0000

    pycodestyle

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import sys
 
     s = 0
-    for i in range (len(sys.argv) - 1):
+    for i in range(len(sys.argv) - 1):
         s += int(sys.argv[i + 1])
-    
+
     print("{}".format(s))
