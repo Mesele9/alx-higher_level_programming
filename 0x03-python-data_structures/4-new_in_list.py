a0a0388b7823e7355a904a81471ff5ab6a42fd02
Mesele <getmesy@gmail.com>
2022-12-05 17:56:05 +0000

    push 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,8 +4,9 @@
 def new_in_list(my_list, idx, element):
     """Replace an element in a list at a specific position without
     modifying the original list"""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
 
     new_list = my_list.copy()
-    if idx > 0 and idx < len(my_list) - 1:
-        new_list[idx] = element
+    new_list[idx] = element
     return new_list
