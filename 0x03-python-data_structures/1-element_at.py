512e7fb998128441c4c137a2d7d0059b0c9e56de
Mesele <getmesy@gmail.com>
2022-12-05 03:11:06 +0000

    return None

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,8 +5,8 @@ def element_at(my_list, idx):
     """a function that retrieves an element from a list."""
 
     if idx < 0:
-        return 0
+        return None
     elif idx >= len(my_list):
-        return 0
+        return None
     else:
         return my_list[idx]
