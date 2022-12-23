9b6230d4c6277422fcf638ce1ed016ea07f99227
Mesele <getmesy@gmail.com>
2022-12-23 02:03:29 +0000

    correct 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,20 +15,20 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        @property
-        def size(self):
-            """Getters Property"""
-            return self.__size
+    @property
+    def size(self):
+        """Getters Property"""
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Setters Property"""
-            if type(value) != int:
-                raise TypeError("size must be an integer")
-            if int(value) < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """Setters Property"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if int(value) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            """The Function that returns the area of the square"""
-            return (self.__size ** 2)
+    def area(self):
+        """The Function that returns the area of the square"""
+        return (self.__size ** 2)
