e327d993418e9e72b331da42a44b317add5bdabb
Mesele <getmesy@gmail.com>
2022-12-23 02:08:42 +0000

    correct 5-sqaure.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -16,29 +16,29 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        @property
-        def size(self):
-            """Getters Property"""
-            return self.__size
+    @property
+    def size(self):
+        """Getters Property"""
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Setters Property"""
-            if type(value) != int:
-                raise TypeError("size must be an integer")
-            if int(value) < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """Setters Property"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if int(value) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            """The Function that returns the area of the square"""
-            return (self.__size ** 2)
+    def area(self):
+        """The Function that returns the area of the square"""
+        return (self.__size ** 2)
 
-        def my_print(self):
-            """A Function that prints the square."""
-            if self.__size == 0:
-                print()
-                for i in range(0, self.__size):
-                    for j in range(0, self.__size):
-                        print("#", end="")
-                    print()
+    def my_print(self):
+        """A Function that prints the square."""
+        if self.__size == 0:
+            print()
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
