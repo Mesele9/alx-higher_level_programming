53c650e40c44ae8f32ee81ae695ec1a4b094d2ba
Mesele <getmesy@gmail.com>
2022-12-23 01:50:29 +0000

    correction on 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,4 +5,4 @@ class Square:
     """A class that defines a square by private instance attribute, size"""
     def __init__(self, size=0):
         """Initialization of class with the size parameter"""
-        self._size = size
+        self.__size = size
