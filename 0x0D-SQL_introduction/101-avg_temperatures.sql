3db40d19a6d03cdb40a5f35182d142c5f5a18dbd
Mesele <getmesy@gmail.com>
2023-02-17 16:55:21 +0000

    push advanced tasks

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- displays the average temperature by city
+-- ordered by temperature descending order
+
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
