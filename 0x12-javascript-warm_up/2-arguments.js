b90ca5fdda6d5f2f6df81150c5e5d663d62a99dc
Mesele <getmesy@gmail.com>
2023-03-14 17:04:36 +0000

    corect

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 let args = process.argv.slice(2);
 
-if (args.length === 0) {
-	console.log('No argument');
-} else {
-       console.log('Arguments found');
-}
+if (args.length === 0) { console.log('No argument'); }
+else { console.log('Arguments found'); }
