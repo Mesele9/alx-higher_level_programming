5fc00ac1e16cbefbe5b5c5d758fa733d1fec7039
Mesele <getmesy@gmail.com>
2023-03-15 10:50:31 +0000

    c

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const arg = process.argv[2];
-const n = parseInt(arg);
-if (isNaN(n)) {
+const arg = parseInt(process.argv[2]);
+
+if (isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log(`My number is: ${n}`);
+  console.log(`My number is: ${arg}`);
 }
