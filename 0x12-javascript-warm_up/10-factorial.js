5aeadb90bac9684b882f1840c544c4b60a2cfb2c
Mesele <getmesy@gmail.com>
2023-03-15 14:27:15 +0000

    c

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 function factorial(a) {
-  if (a === 0 || a === 1) {
+  if (isNaN(a)) {
+    return 1;
+  } else if (a === 0 || a === 1) {
     return 1;
   } else {
     return a * factorial(a - 1);
@@ -9,8 +11,5 @@ function factorial(a) {
 }
 
 const arg = parseInt(process.argv[2]);
-if (isNaN(arg)) {
-  console.log(1);
-} else {
-  console.log(factorial(arg));
+console.log(factorial(arg));
 } 
