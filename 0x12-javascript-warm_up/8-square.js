32617c12c75e7b591dc782a14327160f0113a336
Mesele <getmesy@gmail.com>
2023-03-15 13:40:00 +0000

    c

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arg = parseInt(process.argv[2]);
 if (isNaN(arg)) {
-  console.log('Missing number of occurences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < arg; i++) {
     console.log('X'.repeat(arg));
