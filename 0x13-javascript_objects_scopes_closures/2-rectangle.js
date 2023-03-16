162cb0305cca21edeac5a22a34fe3771436cb62f
Mesele <getmesy@gmail.com>
2023-03-16 18:05:25 +0000

    c

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,7 +4,7 @@
  */
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+    if (w <= 0 || h <= 0 || !(w) || !(h)) {
       return {};
     }
     this.width = w;
