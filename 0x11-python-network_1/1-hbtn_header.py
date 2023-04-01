3ef695ea942f6c13d23b1a45e259fb05931dcca5
Mesele <getmesy@gmail.com>
2023-04-01 02:32:13 +0000

    correction

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,9 @@
 import urllib.request
 import sys
 
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-url = sys.argv[1]
-request = urllib.request.Request(url)
-with urllib.request.urlopen(request) as data:
-    print(data.getheader('X-Request-Id'))
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as data:
+        print(data.getheader('X-Request-Id'))
