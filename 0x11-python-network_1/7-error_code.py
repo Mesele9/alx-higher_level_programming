ac3001d2473cc1390ca55f6752e4ff4375c86355
Mesele <getmesy@gmail.com>
2023-04-02 11:21:37 +0000

    coorection

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
     Python script that takes in a URL, sends a request to the URL and displays
-    the body of the response.
-    If the HTTP status code is greater than or equal to 400, print:
-    Error code: followed by the value of the HTTP status code
+    the body of the response. If the HTTP status code is greater than or equal
+    to 400, print: Error code: followed by the value of the HTTP status code
 """
 import requests
 import sys
@@ -11,8 +10,8 @@ import sys
 
 if __name__ == "__main__":
 
-    response = requests.get(argv[1])
-    if response.status_code >= 400:
-        print("Error code: {}".format(response.status_code))
+    resp = requests.get(argv[1])
+    if resp.status_code >= 400:
+        print("Error code: {}".format(resp.status_code))
     else:
-        print(response.text)
+        print(resp.text)
