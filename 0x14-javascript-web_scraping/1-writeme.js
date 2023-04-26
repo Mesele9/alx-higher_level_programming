affb794f73a04861462ebd7eb45e1e37dd33f22e
Mesele <getmesy@gmail.com>
2023-04-26 17:48:04 +0000

    correction

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,9 @@ const fs = require('fs');
 const file = process.argv[2];
 const strContent = process.argv[3];
 
-fs.writeFile(file, strContent, 'utf8', (err) => {
+fs.writeFile(file, strContent, 'utf-8', (err) => {
   if (err) {
-    console.error(err);
+    console.log(error);
     return;
   }
-  console.log('File written successfully');
 });
