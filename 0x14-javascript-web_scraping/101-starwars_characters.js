ffb2e8be380b0615ecfef64139b6573731fb99f0
Mesele <getmesy@gmail.com>
2023-04-27 01:27:58 +0000

    correction

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,20 +4,28 @@ const request = require('request');
 
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+let characters = [];
 
 request(url, (err, response, body) => {
   if (err) {
     console.log(err);
   }
   const film = JSON.parse(body);
-
-  film.characters.forEach((characterUrl, index) => {
-    request(characterUrl, (err, response, body) => {
-      if (err) {
-        console.log(err);
-      }
-      const character = JSON.parse(body);
-      console.log(`${character.name}`);
-    });
-  });
+  characters = film.characters;
+  getCharacters(0);
 });
+
+const getCharacters = (index) => {
+  if (index === characters.length) {
+    return;
+  }
+
+  request(characters[index], (err, response, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const characterContent = JSON.parse(body);
+    console.log(characterContent.name);
+    getCharacters(index + 1);
+  });
+};
